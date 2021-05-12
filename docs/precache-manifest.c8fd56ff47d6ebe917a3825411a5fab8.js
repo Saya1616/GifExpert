23b8715fb26163f217d8ae3b6995629c1b2cca81
Sayita <Darktobi1616@hotmail.com>
2021-05-11 22:49:40 -0600

    docks

---
+++ docs/precache-manifest.c8fd56ff47d6ebe917a3825411a5fab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eed983ce9bb1be532a5d26fe66405d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8eb229b29b1ba2cf322a",
+    "url": "/static/css/main.3a3c781c.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb229b29b1ba2cf322a",
+    "url": "/static/js/main.4ea0e2f5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
